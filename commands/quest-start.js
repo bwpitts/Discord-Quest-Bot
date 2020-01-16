fa3c1e50bad7602093179d79eaeb3963f2a0680b
Ben <bpitts@moneymappress.com>
2020-01-16 16:22:12 -0500

    saving

--- commands/quest-start.js
+++ commands/quest-start.js
@@ -3,13 +3,8 @@ module.exports = {
 	name: 'quest-start',
 	description: 'This will start a quest for the server.',
 	aliases: ['quest'],
-	execute(message) {
-		let quest = quests['lumber-quest']
-		let questTitle = quest.title;
-		let questDescription = quest.request;
-		let rewardText = "We will reward " + quest.reward + " per " + quest.item;
-		message.channel.send(`**${questTitle}**`);
-		message.channel.send(`${questDescription}`);
-		message.channel.send(rewardText);
+	execute(message, args) {
+		currentQuest = args;
+		message.channel.send(args);
 	},
 };
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+let currentQuest;
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
